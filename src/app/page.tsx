49451b0896fbd63febbf89e0de42518f129d661c
Anderson Laverde <hola@andersonlaverde.com>
2024-02-05 16:53:43 -0500

    remove unused files and load svgs as components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
+import Logo from './next.svg'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
+      <Logo className="stroke-white w-40 h-40 dark:drop-shadow-[0_0_0.3rem_#ffffff70]"  />
     </main>
   );
 }
